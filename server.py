cf67ecfbbff6f4fb86d45369d4709cb953fb757c
G <photodetached@gmail.com>
2026-01-02 16:42:09 -0500

    second commit

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    # return "Hello World!"
      return render_template('index.html')
 
 @app.route('/weather')
@@ -17,7 +16,8 @@ def get_weather():
     # Check for empty strings or string with only spaces
     if not bool(city.strip()):
         # You could render "City not found." instead like below
-        city = "Fairfield"
+        #city = "Fairfield"
+        return render_template('missing.html')
 
     weather_data = get_current_weather(city)
 
@@ -25,16 +25,30 @@ def get_weather():
     # City is not found by the API
     if not weather_data['cod'] == 200:
         return render_template('missing.html')
-
     
-    #return f"{weather_data['main']['feels_like']:.1f}"
-    return render_template(
-        "weather.html",
-        title=weather_data["name"],
-        status=weather_data["weather"][0]["description"].capitalize(),
-        temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['main']['feels_like']:.1f}"
-    )
+    if not weather_data['wind']['gust']:
+        return render_template(
+            "weather1.html",
+            title=weather_data["name"],
+            status=weather_data["weather"][0]["description"].capitalize(),
+            temp=f"{weather_data['main']['temp']:.1f}",
+            feels_like=f"{weather_data['main']['feels_like']:.1f}",
+            humidity=f"{weather_data['main']['humidity']:2.0f}",
+            pressure=f"{weather_data['main']['pressure']:4.0f}",
+            wind_speed=f"{weather_data['wind']['speed']:.1f}"
+            )
+    else:    
+        return render_template(
+            "weather.html",
+            title=weather_data["name"],
+            status=weather_data["weather"][0]["description"].capitalize(),
+            temp=f"{weather_data['main']['temp']:.1f}",
+            feels_like=f"{weather_data['main']['feels_like']:.1f}",
+            humidity=f"{weather_data['main']['humidity']:2.0f}",
+            pressure=f"{weather_data['main']['pressure']:4.0f}",
+            wind_speed=f"{weather_data['wind']['speed']:.1f}",
+            wind_gust=f"{weather_data['wind']['gust']:.1f}"
+            )
     
 
 if __name__ == "__main__":
